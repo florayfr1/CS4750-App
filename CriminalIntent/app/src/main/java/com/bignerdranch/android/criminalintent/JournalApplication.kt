b741f4f26ddb41d044d9c6f856fa0879e17cd9d0
megansmistaken <mglauw@gmail.com>
2020-11-04 00:31:04 -1000

    megan's name changes
    i didn't change pictureutils cause there's no crime stuff there

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/JournalApplication.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/JournalApplication.kt
@@ -2,9 +2,9 @@ package com.bignerdranch.android.criminalintent
 
 import android.app.Application
 
-class CriminalIntentApplication : Application() {
+class JournalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        CrimeRepository.initialize(this)
+        DayRepository.initialize(this)
     }
 }
