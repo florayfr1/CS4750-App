a443a3be8d8aac3aff22f14bc093fce68f3fb553
florayfr1 <florayfr@gmail.com>
2020-11-04 17:28:41 -0800

    name change

---
+++ JournalApp/app/src/androidTest/java/database/DayDatabase.kt
@@ -0,0 +1,26 @@
+package database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.bignerdranch.android.JournalApp.Day
+
+@Database(entities = [ Day::class ], version=2)
+@TypeConverters(DayTypeConverters::class)
+abstract class DayDatabase : RoomDatabase() {
+
+    object migration_1_2 : Migration(1,2) {
+        override fun migrate(database: SupportSQLiteDatabase) {
+            database.execSQL(
+                "ALTER TABLE Day ADD COLUMN suspect TEXT NOT NULL DEFAULT''"
+            )
+        }
+    }
+
+    abstract fun dayDao(): DayDao
+
+
+}
+
