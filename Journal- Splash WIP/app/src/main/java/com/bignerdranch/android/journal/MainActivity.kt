b2d69ce76d9a9f206e36b4c28367ed9c320c088c
florayfr1 <florayfr@gmail.com>
2020-11-16 21:14:32 -0800

    Splash working

--- Journal- Splash WIP/app/src/main/java/com/bignerdranch/android/journal/MainActivity.kt
+++ Journal- Splash WIP/app/src/main/java/com/bignerdranch/android/journal/MainActivity.kt
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import java.util.*
 private  const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity(),
     EntryListFragment.Callbacks{
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
